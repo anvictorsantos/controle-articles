0344a7f740020a85b08aded8808e37e88aa6bac3
Antonio Pontes <antonio.pontes@farmaciaindiana.com.br>
2022-12-13 13:34:43 -0300

    feat: criação do controller e métodos do artigo

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Article;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $articles = Article::latest()->paginate(5);
+
+        return view('article.index', compact('articles'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('article.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        request()->validate([
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+
+        Article::create($request->all());
+
+        return redirect()->route('article.index')
+            ->with('success', 'Artigo criado com sucesso');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $article = Article::find($id);
+
+        return view('article.show', compact('article'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $article = Article::find($id);
+
+        return view('article.edit', compact('article'));        
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        request()->validate([
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+
+        Article::find($id)->update('article.index')
+            ->with('success', 'Artigo atualizado com sucesso');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Article::find($id)->delete();
+
+        return redirect()->route('article.index')
+            ->with('success', 'Artigo deletado com sucesso');
+    }
+}
